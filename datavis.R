47aba209bccfbe82a1b8d93889a999465661b826
miranda-chin <mchin2387@gmail.com>
2025-04-23 10:13:24 -0600

    analysis

--- datavis.R
+++ datavis.R
@@ -27,8 +27,35 @@ ozone_2021 <- ggplot(data = filter_data_2021, aes(x = `Date Local`, y = AQI)) +
 library(patchwork)
 ozone_2003 + ozone_2021
 
-#combine data into one set
-total_data <- rbind(filter_data_2003, filter_data_2021)
+#Load data
+data_2003 <- readr::read_csv("ad_viz_plotval_data.csv")
+data_2004 <- readr::read_csv("ad_viz_plotval_data(1).csv")
+data_2005 <- readr::read_csv("ad_viz_plotval_data(2).csv")
+data_2006 <- readr::read_csv("ad_viz_plotval_data(3).csv")
+data_2007 <- readr::read_csv("ad_viz_plotval_data(4).csv")
+data_2008 <- readr::read_csv("ad_viz_plotval_data(5).csv")
+data_2009 <- readr::read_csv("ad_viz_plotval_data(6).csv")
+data_2010 <- readr::read_csv("ad_viz_plotval_data(7).csv")
+data_2011 <- readr::read_csv("ad_viz_plotval_data(8).csv")
+data_2012 <- readr::read_csv("ad_viz_plotval_data(9).csv")
+data_2013 <- readr::read_csv("ad_viz_plotval_data(10).csv")
+data_2014 <- readr::read_csv("ad_viz_plotval_data(11).csv")
+data_2015 <- readr::read_csv("ad_viz_plotval_data(12).csv")
+data_2016 <- readr::read_csv("ad_viz_plotval_data(13).csv")
+data_2017 <- readr::read_csv("ad_viz_plotval_data(14).csv")
+data_2018 <- readr::read_csv("ad_viz_plotval_data(15).csv")
+data_2019 <- readr::read_csv("ad_viz_plotval_data(16).csv")
+data_2020 <- readr::read_csv("ad_viz_plotval_data(17).csv")
+data_2021 <- readr::read_csv("ad_viz_plotval_data(18).csv")
 
-ggplot(total_data, aes(x = `Date Local`, y = AQI)) +
-  geom_line()
+#combine data
+total_data <- rbind(data_2003, data_2004, data_2005, data_2006, data_2007, 
+                    data_2008, data_2009, data_2010, data_2011, data_2012, 
+                    data_2013, data_2014, data_2015, data_2016, data_2017,
+                    data_2018, data_2019, data_2020, data_2021)
+#visualization
+ggplot(data = total_data, aes(x = `Date`, y = `Daily AQI Value`)) +
+  geom_col() +
+  labs(title = "Larimer AQI Over Time", x = "Date", y = "AQI")
+
+
